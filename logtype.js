6f74631368eaaf8db560f65e0edb49c6db69fa10
GEJO-PC <roshni2983@gmail.com>
2018-07-10 12:53:49 +0200

    Exercise 1: Function to Log type of argument passed

---
+++ logtype.js
@@ -0,0 +1,35 @@
+/*
+Write a function named logType that expects a single argument and logs a different string depending on the type/value of the argument that is passed to it. The string it logs should be one of the following:
+"undefined!"
+"null!"
+"number!"
+"not a number!"
+"string!"
+"boolean!"
+"function!"
+"object!"
+"array!"
+"I have no idea!" */
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function LogType(arg) {
+    var type = typeof arg;
+    if (isNaN(arg) === true && type === "number") {
+        console.log("not a number!");
+    } else if (type === "object" && arg === null) {
+        console.log("null!");
+    } else {
+        console.log('"' + type + '!"');
+    }
+}
+var x;
+var nn = NaN;
+LogType("hello");
+LogType({});
+LogType([10, 20, 30]);
+LogType(null);
+LogType(true);
+LogType(x);
+LogType(function f() {});
+LogType(nn);
