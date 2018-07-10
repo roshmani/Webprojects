49e07f0e23ac5f1de935ed7e95b52fd94ab81d69
GEJO-PC <roshni2983@gmail.com>
2018-07-10 11:54:43 +0200

    Exercise 3: Printing numbers to console

---
+++ printnumbers.js
@@ -0,0 +1,9 @@
+/*Write a while or for loop that counts down from 10 to 1, logging each number to the console.*/
+
+function printNumbers() {
+    for (var num = 10; num >= 1; num--) {
+        console.log(num);
+    }
+}
+
+printNumbers();
