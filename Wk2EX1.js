b64566126dbecd70864d9cdf21dcf80c8fdc67b6
GEJO-PC <roshni2983@gmail.com>
2018-07-16 12:13:13 +0200

    Week 2 . Exercise 1 and 2

---
+++ Wk2EX1.js
@@ -0,0 +1,14 @@
+/*Write a function that expects a string representing a selector
+to be passed as a parameter. The function should find all the elements
+in the document that match the selector and change their style
+so that the text they contain is italic, underlined, and bold.*/
+function changeStyling(selectorarg) {
+    var elems = document.querySelectorAll(selectorarg);
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].style.fontStyle = "italic";
+        elems[i].style.textDecoration = "underline";
+        elems[i].style.fontWeight = "bold";
+    }
+}
+
+window.onLoad = changeStyling("section");
