5259b040f9ef1cddfaab1d2ba5769bd472759544
GEJO-PC <roshni2983@gmail.com>
2018-07-18 10:41:50 +0200

    adding back deleted .js file

---
+++ Ticker/script.js
@@ -0,0 +1,34 @@
+(function() {
+    var animId;
+    var box = document.getElementById("box");
+    var initSpaceBoxLeft = box.offsetLeft;
+    var spaceBoxLeft = initSpaceBoxLeft;
+    var newsList = document.querySelectorAll("a");
+    var lastIndex = newsList.length - 1;
+    var widthLastItem = newsList[lastIndex].offsetWidth;
+    var totalWidth = widthLastItem + box.offsetWidth;
+    function move() {
+        if (spaceBoxLeft >= -totalWidth) {
+            spaceBoxLeft--;
+            box.style.left = spaceBoxLeft + "px";
+        } else {
+            spaceBoxLeft = initSpaceBoxLeft;
+        }
+        animId = requestAnimationFrame(move);
+    }
+    move();
+
+    var linkelems = document.getElementsByClassName("news");
+    var boxelem = document.getElementById("box");
+
+    Array.prototype.forEach.call(linkelems, function(link) {
+        link.addEventListener("mouseenter", function() {
+            cancelAnimationFrame(animId);
+            link.style.textDecoration = "underline";
+            link.style.color = "blue";
+        });
+    });
+    boxelem.addEventListener("mouseleave", function() {
+        move();
+    });
+})();
