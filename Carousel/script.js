caa0e7be3e741bc0a956f6ae7e6292f5c2dd4183
GEJO-PC <roshni2983@gmail.com>
2018-07-21 23:58:42 +0200

    Tried out touch events for swipe-Bonus ,changed the kitty pics and fit to scale-down

--- Carousel/script.js
+++ Carousel/script.js
@@ -5,6 +5,7 @@
     var currkitty = 0;
     var timer;
     var isTransitionOn = false; //flag for checking if it is doing something already
+    var touchstart, touchend, idx;
     function moveKitties(next) {
         kitties[currkitty].classList.remove("onscreen");
         kitties[currkitty].classList.add("exit");
@@ -35,7 +36,7 @@
         dot.addEventListener("click", function() {
             console.log("click called");
             if (currkitty == i) {
-                console.log("same");
+                console.log("on same image");
                 return;
             } else if (isTransitionOn == true) {
                 console.log("on transition");
@@ -47,5 +48,43 @@
             }
         });
     });
+    var curImg = [].slice.call(kitties);
+    console.log(curImg);
+    /*Testing touch events*/
+    for (idx = 0; idx < curImg.length; idx++) {
+        console.log(curImg[idx] + " " + idx);
+        curImg[idx].addEventListener("touchstart", handleStart, false);
+        curImg[idx].addEventListener("touchend", handleEnd, false);
+    }
+    function handleStart(evt) {
+        evt.preventDefault();
+        touchstart = evt.changedTouches[0].screenX;
+        console.log("touchstart:" + evt.target + "at:" + touchstart);
+    }
+    function handleEnd(evt) {
+        evt.preventDefault();
+        touchend = evt.changedTouches[0].screenX;
+        console.log("touchend:" + evt.target + "at:" + touchend);
+        clearTimeout(timer);
+        console.log("touchstart:" + touchstart);
+        if (touchstart < touchend) {
+            if (idx > 0) {
+                idx -= 1;
+                console.log("index:prev:" + idx);
+            } else {
+                idx = 3;
+            }
+            moveKitties(idx);
+        } else {
+            if (idx < 3) {
+                idx += 1;
+                console.log("index:next:" + idx);
+            } else {
+                idx = 0;
+            }
+            moveKitties(idx);
+        }
+    }
+
     setTimeout(moveKitties, 2500);
 })();
