0242130456320466e027b1b5f01ce6a607cc0462
GEJO-PC <roshni2983@gmail.com>
2018-07-21 23:59:35 +0200

    Could solve speed of animation getting fast

--- Ticker/script.js
+++ Ticker/script.js
@@ -6,7 +6,8 @@
     var newsList = document.querySelectorAll("a");
     var lastIndex = newsList.length - 1;
     var widthLastItem = newsList[lastIndex].offsetWidth;
-    var totalWidth = widthLastItem + box.offsetWidth;
+    var totalWidth =
+        widthLastItem + box.offsetWidth - (widthLastItem + initSpaceBoxLeft);
     function move() {
         if (spaceBoxLeft >= -totalWidth) {
             spaceBoxLeft--;
@@ -22,13 +23,15 @@
     var boxelem = document.getElementById("box");
 
     Array.prototype.forEach.call(linkelems, function(link) {
-        link.addEventListener("mouseenter", function() {
+        link.addEventListener("mouseenter", function(e) {
+            e.stopPropagation();
             cancelAnimationFrame(animId);
-            link.style.textDecoration = "underline";
-            link.style.color = "blue";
         });
     });
     boxelem.addEventListener("mouseleave", function() {
+        if (animId) {
+            cancelAnimationFrame(animId);
+        }
         move();
     });
 })();
