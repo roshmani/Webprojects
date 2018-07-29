988e06dd4069742c710d33fa0167489867657b09
GEJO-PC <roshni2983@gmail.com>
2018-07-29 23:08:32 +0200

    added a header and code to indicate win position

--- connect4/script.js
+++ connect4/script.js
@@ -2,7 +2,7 @@
     var curPlayer = "player1";
     var curColor = "red";
     var slot = $(".slot");
-
+    var matchindex;
     var doNothingFlag = false;
     $(".fall").css("background-color", curColor);
     $(".column").on("click", function(e) {
@@ -21,16 +21,25 @@
             slotsInCol.eq(i).addClass(curPlayer);
             var curfall = $(e.currentTarget).find(".fall");
             var curhole = slotsInCol.eq(i).find(".hole");
-            var offsetY = curhole.offset().top - 15;
+            var offsetY = curhole.offset().top - 78;
+            console.log("holeoffset", offsetY);
             curfall.css("background-color", curColor);
             curfall.css("visibility", "visible");
             var translatey = "translateY(" + offsetY + "px)";
             curfall.css("transform", translatey);
             if (checkForVictory(slotsInCol)) {
+                for (i = matchindex; i < matchindex + 4; i++) {
+                    var holeCur = slotsInCol.eq(i).children();
+                    fadeHole(holeCur);
+                }
                 return showVictoryMessage();
             } else {
                 var slotsInRow = $(".row" + i);
                 if (checkForVictory(slotsInRow)) {
+                    for (i = matchindex; i < matchindex + 4; i++) {
+                        holeCur = slotsInRow.eq(i).children();
+                        fadeHole(holeCur);
+                    }
                     return showVictoryMessage();
                 } else {
                     if (checkDiagonalVictory()) {
@@ -54,11 +63,13 @@
                 winCheck += "n";
             }
         }
-        return winCheck.indexOf("yyyy") > -1;
+        matchindex = winCheck.indexOf("yyyy");
+        return matchindex > -1;
     }
 
     function checkDiagonalVictory() {
         // descendingDiagonalCheck
+        var diagArray = [];
         var numCols = $(".column").length;
         var numRows = $(".column").find(".slot").length / numCols;
         for (var i = 3; i < numRows; i++) {
@@ -80,8 +91,37 @@
                         .find(".slot")
                         .eq(i - 3)
                         .hasClass(curPlayer)
-                )
+                ) {
+                    // pushing diagonal elements of the winning game
+                    diagArray.push(
+                        $("#column" + j)
+                            .find(".slot")
+                            .eq(i)
+                    );
+                    diagArray.push(
+                        $("#column" + (j - 1))
+                            .find(".slot")
+                            .eq(i - 1)
+                    );
+                    diagArray.push(
+                        $("#column" + (j - 2))
+                            .find(".slot")
+                            .eq(i - 2)
+                    );
+                    diagArray.push(
+                        $("#column" + (j - 3))
+                            .find(".slot")
+                            .eq(i - 3)
+                    );
+
+                    //setting a fade to it
+                    for (var d = 0; d < diagArray.length; d++) {
+                        console.log("in fade");
+                        var holeCur = diagArray[d].children();
+                        fadeHole(holeCur);
+                    }
                     return true;
+                }
             }
         }
         // ascendingDiagonalCheck
@@ -105,10 +145,43 @@
                         .eq(i - 3)
                         .hasClass(curPlayer)
                 ) {
+                    diagArray.push(
+                        $("#column" + j)
+                            .find(".slot")
+                            .eq(i)
+                    );
+                    diagArray.push(
+                        $("#column" + (j + 1))
+                            .find(".slot")
+                            .eq(i - 1)
+                    );
+                    diagArray.push(
+                        $("#column" + (j + 2))
+                            .find(".slot")
+                            .eq(i - 2)
+                    );
+                    diagArray.push(
+                        $("#column" + (j + 3))
+                            .find(".slot")
+                            .eq(i - 3)
+                    );
+                    console.log("diagArray:", diagArray.length);
+                    for (var n = 0; n < diagArray.length; n++) {
+                        console.log("in fade");
+                        holeCur = diagArray[n].children();
+                        fadeHole(holeCur);
+                        console.log(diagArray[n]);
+                    }
                     return true;
                 }
             }
         }
+    }
+    function fadeHole(holetoFade) {
+        holetoFade.fadeIn(1500, function() {
+            holetoFade.css("border", "7px solid white");
+        });
+        //holetoFade.fadeTo(100, 0.1).fadeTo(200, 1.0);
     }
     function showVictoryMessage() {
         var modalDlg = $("#modalmsg");
@@ -135,5 +208,20 @@
     });
     $(".newgame").click(function() {
         location.reload();
+    });
+    $(document).ready(function() {
+        $(".dot").hide();
+        $("#one")
+            .delay(1000)
+            .fadeIn(500);
+        $("#two")
+            .delay(1500)
+            .fadeIn(500);
+        $("#three")
+            .delay(2000)
+            .fadeIn(500);
+        $("#four")
+            .delay(2500)
+            .fadeIn(500);
     });
 })(); //end of iife
