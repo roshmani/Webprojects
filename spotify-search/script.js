e13387d33c219c6ac31752784e84116f3c456604
GEJO-PC <roshni2983@gmail.com>
2018-07-31 00:26:55 +0200

    load more results added

--- spotify-search/script.js
+++ spotify-search/script.js
@@ -1,54 +1,88 @@
 (function() {
     var resultheader = "";
     var nextUrl = "";
-    var baseurl = "https://elegant-croissant.glitch.me/spotify";
-    $("#submit-button").on("click", function() {
-        var htmlStr = "";
+    var initUrl = "https://elegant-croissant.glitch.me/spotify";
+    var baseurl = "";
+    var spotifyNext = "";
+    var htmlStr = "";
+    var loadmore = $("#loadmore");
+    $("#submit-button").on("click", function(e) {
+        baseurl = initUrl;
+        fetchData(e.currentTarget);
+    }); //on.click
+
+    loadmore.on("click", function(e) {
+        baseurl = nextUrl;
+        fetchData(e.currentTarget);
+    });
+
+    function fetchData(curTarget) {
+        htmlStr = "";
         var userInput = $("input").val();
         var selType = $("select").val();
-        resultheader = "Results for the search for" + " " + userInput + ":";
-        $("#resultheader").text(resultheader);
-        $("#resultheader").css("visibility", "visible");
-        $.ajax({
-            url: baseurl,
-            data: {
-                query: userInput,
-                type: selType
-            },
-            success: function(artistData) {
-                artistData = artistData.artists || artistData.albums;
-                /*var nextUrl =
-                    artistData.next &&
-                    artistData.next.replace(
-                        "spotify url",
-                        "elegant-croissant url"
-                    );*/
-                for (var i = 0; i < artistData.items.length; i++) {
-                    //console.log("data", artistData.items[i]);
-                    var artistname = artistData.items[i].name;
-                    var imgUrl = "imagesinger.jpg";
-                    var imgHeight = "200px";
-                    var imgWidth = "200px";
-                    var spotifyUrl = "";
-                    if (artistData.items[i].images.length > 0) {
-                        imgUrl = artistData.items[i].images[0].url;
+        if (userInput.length > 0) {
+            resultheader = "Results for the search for" + " " + userInput + ":";
+            $.ajax({
+                url: baseurl,
+                data: {
+                    query: userInput,
+                    type: selType
+                },
+                success: function(artistData) {
+                    artistData = artistData.artists || artistData.albums;
+                    //console.log("data:", artistData);
+                    for (var i = 0; i < artistData.items.length; i++) {
+                        //console.log("data", artistData.items[i]);
+                        var artistname = artistData.items[i].name;
+                        var imgUrl = "imagesinger.jpg";
+                        var imgHeight = "200px";
+                        var imgWidth = "200px";
+                        var spotifyUrl = "";
+                        if (artistData.items[i].images.length > 0) {
+                            imgUrl = artistData.items[i].images[0].url;
+                        }
+                        spotifyUrl =
+                            artistData.items[i].external_urls["spotify"];
+                        htmlStr +=
+                            "<a href=" +
+                            spotifyUrl +
+                            '><div class="result-item"><img id="artistimg" src=' +
+                            imgUrl +
+                            ' alt="singer_albumimg" height=' +
+                            imgHeight +
+                            " width=" +
+                            imgWidth +
+                            ' ><p class="artistName">' +
+                            artistname +
+                            "</p></div></a>";
+                    } //for loop
+
+                    if (curTarget.id == "submit-button") {
+                        $(".results").html(htmlStr);
+                    } else {
+                        $(".results").append(htmlStr);
                     }
-                    spotifyUrl = artistData.items[i].external_urls["spotify"];
-                    htmlStr +=
-                        "<a href=" +
-                        spotifyUrl +
-                        '><div class="result-item"><img id="artistimg" src=' +
-                        imgUrl +
-                        ' alt="singer_albumimg" height=' +
-                        imgHeight +
-                        " width=" +
-                        imgWidth +
-                        ' ><p class="artistName">' +
-                        artistname +
-                        "</p></div></a>";
-                } //for loop
-                $(".results").html(htmlStr);
-            } //function:success
-        }); //.ajax
-    }); //on.click
+                    spotifyNext = artistData.next;
+                    if (spotifyNext !== null) {
+                        nextUrl =
+                            initUrl +
+                            spotifyNext.substring(
+                                spotifyNext.indexOf("?"),
+                                spotifyNext.length
+                            );
+                        $("#loadmore").css("visibility", "visible");
+                    } else {
+                        $("#loadmore").css("visibility", "hidden");
+                    }
+                    if (htmlStr === "") {
+                        resultheader += " No results";
+                    } //if empty
+                    $("#resultheader").text(resultheader);
+                    $("#resultheader").css("visibility", "visible");
+                } //function:success
+            }); //.ajax
+        } else {
+            alert("Please enter artist or album name!");
+        }
+    }
 })(); //iife
