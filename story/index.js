6c0808e3210392d17232b9425cdf1d225afc607b
GEJO-PC <roshni2983@gmail.com>
2018-08-02 15:48:56 +0200

    story init

---
+++ story/index.js
@@ -0,0 +1,47 @@
+const readline = require("readline");
+const chalk = require("chalk");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let story = {
+    q: "Welcome to The Land Of Wizards! Would you like to play?",
+    answers: {
+        yes: {
+            q:
+                "You are alone in a dark forest and facing a fork in the road. Which direction do you turn?",
+            answers: {
+                left: {
+                    q:
+                        "There's a scary wizard! He asks you a tough question. What's 1+1?",
+                    answers: {
+                        "2": "congratulations!"
+                    }
+                },
+                right: "This was not the right choice. Goodbye!"
+            }
+        },
+        no: "Alright then. Enjoy your day!"
+    }
+};
+
+function askQuestion(adventure) {
+    rl.question(adventure.q, function(answer) {
+        //checks the key in the object
+        if (adventure.answers[answer]) {
+            if (typeof adventure.answers[answer] == "object") {
+                const newAdventure = adventure.answers[answer];
+                askQuestion(newAdventure);
+            } else {
+                console.log(adventure.answers[answer]);
+                rl.close();
+            }
+        } else {
+            askQuestion(adventure);
+        }
+    });
+}
+
+askQuestion(story);
