6762e7c18bde1f693b5920d8ef001460e589802e
GEJO-PC <roshni2983@gmail.com>
2018-08-06 16:23:25 +0200

    bonus added git add httprequestlistener/index.js

--- httprequestlistener/index.js
+++ httprequestlistener/index.js
@@ -14,53 +14,57 @@ server.on("request", (request, response) => {
         request.method + "\n" + "request headers:",
         request.headers
     );
-
-    if (request.method == "GET" || request.method == "HEAD") {
-        response.setHeader("Content-Type", "text/html");
-        response.statusCode = 200;
-    }
-    if (request.method == "GET") {
-        console.log("get");
-        response.write(`<!doctype html>
-                        <html>
-                            <title>Hello World!</title>
-                            <p>Hello World!</p>
-                        </html>`);
-        response.end();
-    } else if (request.method == "POST") {
-        console.log("post");
-        response.statusCode = 302;
-        response.setHeader("Location", "/");
-        let body = "";
-        request
-            .on("data", function(chunk) {
-                body += chunk;
-            })
-            .on("end", function() {
-                console.log(body); //logs the entire request body
-                response.end();
-            });
-    } else if (request.method == "HEAD") {
-        console.log("head");
-        response.end();
+    if (request.url == "/request.txt") {
+        const rdstr = fs.createReadStream(__dirname + "/requests.txt");
+        rdstr.pipe(response);
     } else {
-        response.statusCode = 405;
-        response.end();
-    }
-    let logMsg = [
-        new Date(),
-        request.method,
-        request.url,
-        request.headers["user-agent"],
-        "\n"
-    ].join("\t");
-    fs.appendFile(__dirname + "/requests.txt", logMsg, err => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('The "data to append" was appended to file!');
+        if (request.method == "GET" || request.method == "HEAD") {
+            response.setHeader("Content-Type", "text/html");
+            response.statusCode = 200;
         }
-    });
+        if (request.method == "GET") {
+            console.log("get");
+            response.write(`<!doctype html>
+                            <html>
+                                <title>Hello World!</title>
+                                <p>Hello World!</p>
+                            </html>`);
+            response.end();
+        } else if (request.method == "POST") {
+            console.log("post");
+            response.statusCode = 302;
+            response.setHeader("Location", "/");
+            let body = "";
+            request
+                .on("data", function(chunk) {
+                    body += chunk;
+                })
+                .on("end", function() {
+                    console.log(body); //logs the entire request body
+                    response.end();
+                });
+        } else if (request.method == "HEAD") {
+            console.log("head");
+            response.end();
+        } else {
+            response.statusCode = 405;
+            response.end();
+        }
+        let logMsg = [
+            new Date(),
+            request.method,
+            request.url,
+            request.headers["user-agent"],
+            "\n"
+        ].join("\t");
+        fs.appendFile(__dirname + "/requests.txt", logMsg, err => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('The "data to append" was appended to file!');
+            }
+        });
+    }
 });
 /* You can pass a request listener (a function that handles requests) to that function.*/
 server.listen(8080, () => console.log("Listening on local host...."));
