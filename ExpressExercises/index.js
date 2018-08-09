03306ab45a0272312bd5c31c3c628ff6d2591f30
GEJO-PC <roshni2983@gmail.com>
2018-08-09 17:07:56 +0200

    initial version for express handlebars exercises

--- ExpressExercises/index.js
+++ ExpressExercises/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 const ca = require("chalk-animation");
+const fs = require("fs");
 app.use(require("cookie-parser")());
 app.use(
     require("body-parser").urlencoded({
         extended: false
     })
 ); // used in POST requests
-
 const basicAuth = require("basic-auth");
+const hb = require("express-handlebars");
+app.engine("handlebars", hb());
+app.set("view engine", "handlebars");
+
 var auth = function(req, res, next) {
     var creds = basicAuth(req);
     if (!creds || creds.name != "discoduck" || creds.pass != "opensesame") {
@@ -33,12 +37,12 @@ app.use((request, response, next) => {
 });
 
 app.use(express.static("projects"));
-
+app.use(express.static("static"));
 app.get("/cookie", (request, response) => {
     response.send(`
                <h3> Please Accept Cookies to proceed!</h3>
                <form method='POST' action='/cookie'>
-                   <label>Agreed to accept cookies<input type="checkbox" name="acceptcookie" value="Agreement to accept"/></label>
+                   <label>Privacy Policy- accept cookies to proceed<input type="checkbox" name="acceptcookie" value="Agreement to accept"/></label>
                    <button type='submit'>submit</button>
                 </form>
                        `);
@@ -51,4 +55,50 @@ app.post("/cookie", (request, response) => {
         response.redirect("/cookie");
     }
 });
+
+let projectList = fs.readdirSync(__dirname + "/projects");
+const projlist = projectList.map(dir => {
+    const { title, desc } = require(__dirname +
+        "/projects/" +
+        dir +
+        "/info.json");
+    return {
+        dir,
+        title,
+        desc
+    };
+});
+
+app.get("/portfolio", function(request, response) {
+    response.render("welcome", {
+        projectlist: projlist
+    });
+});
+app.get("/projects/:projectName", function(request, response) {
+    //loop through the array and find project Projname-request.params
+    let projname = request.params.projectName;
+    let title,
+        desc,
+        noprojflag = false;
+    for (let i = 0; i < projectList.length; i++) {
+        if (projname === projectList[i]) {
+            title = projlist[i].title;
+            desc = projlist[i].desc;
+            noprojflag = true;
+            break;
+        } else {
+            noprojflag = false;
+        }
+    }
+    if (noprojflag === false) {
+        console.log("No such project");
+    }
+    response.render("projectdescription", {
+        projectList: projectList,
+        projname: projname,
+        title: title,
+        desc: desc
+    });
+});
+
 app.listen(8080, () => ca.rainbow("listening on port 8080..."));
