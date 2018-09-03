4ad6d65276fb60d9263f3dce81a2ac153ead7e1e
roshni <roshni.mani@gmail.com>
2018-09-03 21:56:32 +0200

    Solar system with sun,moon,earth and mars

--- threejs/public/script.js
+++ threejs/public/script.js
@@ -13,26 +13,57 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 var light = new THREE.AmbientLight(0xffffff);
 scene.add(light);
 /**************************************************************************************************************/
-/*                                               Earth                                                        */
+/*                                               Sun                                                          */
 /**************************************************************************************************************/
 var geometry = new THREE.SphereGeometry(10, 32, 32);
 var material = new THREE.MeshPhongMaterial();
+material.map = new THREE.TextureLoader().load("./sunimage.jpg");
+var sunMesh = new THREE.Mesh(geometry, material);
+scene.add(sunMesh);
+/**************************************************************************************************************/
+/*                                               Earth                                                       */
+/**************************************************************************************************************/
+var geometry = new THREE.SphereGeometry(3, 16, 16);
+var material = new THREE.MeshPhongMaterial();
 material.map = new THREE.TextureLoader().load("./earthmap4k.jpg");
 var earthMesh = new THREE.Mesh(geometry, material);
+earthMesh.position.x = sunMesh.position.x - 20;
 scene.add(earthMesh);
+/* added a pivot and added earth to that pivot*/
+var earthPivot = new THREE.Object3D();
+scene.add(earthPivot);
+earthPivot.add(earthMesh);
 /**************************************************************************************************************/
 /*                                               Moon                                                         */
 /**************************************************************************************************************/
-var geometry = new THREE.SphereGeometry(2, 10, 10);
+var geometry = new THREE.SphereGeometry(0.5, 32, 32);
 var material = new THREE.MeshPhongMaterial();
 material.map = new THREE.TextureLoader().load("./moonmap4k.jpg");
 var moonMesh = new THREE.Mesh(geometry, material);
-moonMesh.position.x = earthMesh.position.x - 20;
+moonMesh.position.x = earthMesh.position.y - 5;
 scene.add(moonMesh);
 /* added a pivot and added moon to that pivot*/
-var earthPivot = new THREE.Object3D();
-scene.add(earthPivot);
-earthPivot.add(moonMesh);
+var moonPivot = new THREE.Object3D();
+moonPivot.position.x = earthMesh.position.x;
+scene.add(moonPivot);
+moonPivot.add(moonMesh);
+earthPivot.add(moonPivot);
+
+/**************************************************************************************************************/
+/*                                               Mars                                                         */
+/**************************************************************************************************************/
+var geometry = new THREE.SphereGeometry(0.5, 16, 16);
+var material = new THREE.MeshPhongMaterial();
+material.map = new THREE.TextureLoader().load("./marsmap1k.jpg");
+var marsMesh = new THREE.Mesh(geometry, material);
+marsMesh.position.x = sunMesh.position.x - 30;
+marsMesh.position.y = earthMesh.position.y + 5;
+scene.add(marsMesh);
+/* added a pivot and added mars to that pivot*/
+var marsPivot = new THREE.Object3D();
+scene.add(marsPivot);
+marsPivot.add(marsMesh);
+/*************************************Orbit Controls*************************************************************/
 var orbit = new THREE.OrbitControls(camera, renderer.domElement);
 orbit.enableZoom = false;
 var imagePrefix = "./";
@@ -46,15 +77,16 @@ var urls = [
 ];
 var skyBox = new THREE.CubeTextureLoader().setPath(imagePrefix).load(urls);
 scene.background = skyBox;
-var time = new Date();
-var moonOrbitAngle = 0;
+
 var render = function() {
     requestAnimationFrame(render);
-    moonOrbitAngle += 0.005;
-    var radians = (moonOrbitAngle * Math.PI) / 180;
-    earthMesh.rotation.x += 0.005;
-    earthMesh.rotation.y += 0.005;
+
+    sunMesh.rotation.x += 0.005;
+    sunMesh.rotation.y += 0.005;
+    marsPivot.rotation.y += 0.005;
     earthPivot.rotation.y += 0.005;
+    moonPivot.rotation.y += 0.015;
+
     renderer.render(scene, camera);
 };
 render();
